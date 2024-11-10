ff2daeb2233009aec6bd97d98e2d95534498c507
mikastiv <mikastiv@outlook.com>
2024-11-10 00:15:52 -0500

    fix memory leaks

--- src/Sema.zig
+++ src/Sema.zig
@@ -1754,8 +1754,12 @@ fn analyzeBodyInner(
                 const err_union = try sema.resolveInst(extra.data.operand);
                 const err_union_ty = sema.typeOf(err_union);
                 if (err_union_ty.zigTypeTag(zcu) != .error_union) {
-                    const msg = try sema.errMsg(operand_src, "expected error union type, found '{}'", .{err_union_ty.fmt(pt)});
-                    try sema.errNote(operand_src, msg, "consider omitting 'try'", .{});
+                    const msg = msg: {
+                        const msg = try sema.errMsg(operand_src, "expected error union type, found '{}'", .{err_union_ty.fmt(pt)});
+                        errdefer msg.destroy(sema.gpa);
+                        try sema.errNote(operand_src, msg, "consider omitting 'try'", .{});
+                        break :msg msg;
+                    };
                     return sema.failWithOwnedErrorMsg(block, msg);
                 }
                 const is_non_err = try sema.analyzeIsNonErrComptimeOnly(block, operand_src, err_union);
@@ -19840,8 +19844,12 @@ fn zirTry(sema: *Sema, parent_block: *Block, inst: Zir.Inst.Index) CompileError!
     const pt = sema.pt;
     const zcu = pt.zcu;
     if (err_union_ty.zigTypeTag(zcu) != .error_union) {
-        const msg = try sema.errMsg(operand_src, "expected error union type, found '{}'", .{err_union_ty.fmt(pt)});
-        try sema.errNote(operand_src, msg, "consider omitting 'try'", .{});
+        const msg = msg: {
+            const msg = try sema.errMsg(operand_src, "expected error union type, found '{}'", .{err_union_ty.fmt(pt)});
+            errdefer msg.destroy(sema.gpa);
+            try sema.errNote(operand_src, msg, "consider omitting 'try'", .{});
+            break :msg msg;
+        };
         return sema.failWithOwnedErrorMsg(parent_block, msg);
     }
     const is_non_err = try sema.analyzeIsNonErrComptimeOnly(parent_block, operand_src, err_union);
@@ -19900,8 +19908,12 @@ fn zirTryPtr(sema: *Sema, parent_block: *Block, inst: Zir.Inst.Index) CompileErr
     const pt = sema.pt;
     const zcu = pt.zcu;
     if (err_union_ty.zigTypeTag(zcu) != .error_union) {
-        const msg = try sema.errMsg(operand_src, "expected error union type, found '{}'", .{err_union_ty.fmt(pt)});
-        try sema.errNote(operand_src, msg, "consider omitting 'try'", .{});
+        const msg = msg: {
+            const msg = try sema.errMsg(operand_src, "expected error union type, found '{}'", .{err_union_ty.fmt(pt)});
+            errdefer msg.destroy(sema.gpa);
+            try sema.errNote(operand_src, msg, "consider omitting 'try'", .{});
+            break :msg msg;
+        };
         return sema.failWithOwnedErrorMsg(parent_block, msg);
     }
     const is_non_err = try sema.analyzeIsNonErrComptimeOnly(parent_block, operand_src, err_union);
